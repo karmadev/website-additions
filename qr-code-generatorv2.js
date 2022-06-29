d559eeaaa5da489898d2c95c3d592ac3409dca39
Hjalmar Ståhlberg Nordegren <hjalmarsn@me.com>
2022-06-29 14:52:42 +0200

    Added canvas sizing, dynamic design generation

--- qr-code-generatorv2.js
+++ qr-code-generatorv2.js
@@ -1,9 +1,11 @@
+//Yes I know this is hideous, but I don't have anything other than vanilla js :D Please make this recursive so it gets all json files in a directlry instead
+
 let canvas = document.getElementById("qr-code");
 let ctx = canvas.getContext("2d");
 
 //Get the width & height of the canvas
-const size = canvas.width;
-const radius = size/2;
+let size = canvas.width;
+let radius = size/2;
 
 let sizeMultiplier = 3.527777366203752; //From 283px to 1000 in this tool!
 let centerRadiusOffset = 3.2;
@@ -11,86 +13,168 @@ let centerRadiusOffset = 3.2;
 let imageLogo;
 let karmaLogo;
 
+//Load all the templates
+//fetch('/templates/circle.json').then((data) => data.json()).then(json => defaults = { ...defaults, "circle" : json });
+
 let defaults = {
-    "circle" : {
-        shape: {
-            type: "circle"
+    "circle":
+        {
+            "name": "Acrylic circle",
+            "print_size": "10cm x 10cm",
+            "canvas_w": 1000,
+            "canvas_h": 1000,
+            "shape": {
+                "type": "circle"
+            },
+            "logo": {
+                "w": 200,
+                "h": 100,
+                "offset_y": 0.09,
+                "offset_x": 0.5
+            },
+            "karmaLogo": {
+                "w": 200,
+                "h": 50,
+                "offset_y": 0.95,
+                "offset_x": 0.5
+            },
+            "title": {
+                "offset_x": 0.5,
+                "offset_y": 0.275,
+                "fontsize": 40
+            },
+            "subtitle": {
+                "offset_x": 0.5,
+                "offset_y": 0.49,
+                "fontsize": 12
+            },
+            "tableTitle": {
+                "offset_x": 0.66,
+                "offset_y": 0.6,
+                "fontsize": 15.66
+            },
+            "tableNumber": {
+                "offset_x": 0.66,
+                "offset_y": 0.75,
+                "fontsize": 47.45
+            },
+            "bottomTitle": {
+                "offset_x": 0.5,
+                "offset_y": 0.89,
+                "fontsize": 10
+            },
+            "QRCode": {
+                "size": 76.235,
+                "offset_x": 0.33,
+                "offset_y": 0.68
+            }
         },
-        logo: {
-            w: 200,
-            h: 100,
-            offset_y: 40
+    "square1":
+        {
+            "name": "Acrylic square",
+            "print_size": "10cm x 10cm",
+            "canvas_w": 1000,
+            "canvas_h": 1000,
+            "shape": {
+                "type": "rect",
+                "radius": 140
+            },
+            "logo": {
+                "w": 200,
+                "h": 100,
+                "offset_y": 0.08,
+                "offset_x": 0.5
+            },
+            "karmaLogo": {
+                "w": 200,
+                "h": 50,
+                "offset_y": 0.95,
+                "offset_x": 0.5
+            },
+            "title": {
+                "offset_x": 0.5,
+                "offset_y": 0.275,
+                "fontsize": 40
+            },
+            "subtitle": {
+                "offset_x": 0.5,
+                "offset_y": 0.49,
+                "fontsize": 12
+            },
+            "tableTitle": {
+                "offset_x": 0.66,
+                "offset_y": 0.6,
+                "fontsize": 16
+            },
+            "tableNumber": {
+                "offset_x": 0.66,
+                "offset_y": 0.75,
+                "fontsize": 49
+            },
+            "bottomTitle": {
+                "offset_x": 0.5,
+                "offset_y": 0.89,
+                "fontsize": 10
+            },
+            "QRCode": {
+                "size": 76.235,
+                "offset_x": 0.33,
+                "offset_y": 0.68
+            }
         },
-        title: {
-            offset_x: radius,
-            offset_y: radius-225,
-            fontsize: 40
-        },
-        subtitle : {
-            offset_x: radius,
-            offset_y: radius-10,
-            fontsize: 12
-        },
-        tableTitle : {
-            offset_x: radius + radius/centerRadiusOffset,
-            offset_y: radius+100,
-            fontsize: 15.66
-        },
-        tableNumber : {
-            offset_x: radius + radius/centerRadiusOffset,
-            offset_y: radius+250,
-            fontsize: 47.45
-        },
-        bottomTitle : {
-            offset_x: radius,
-            offset_y: radius*2-110,
-            fontsize: 10
-        },
-        QRCode : {
-            offset_x: radius,
-            offset_y: radius-10,
+    "a6":
+        {
+            "name": "A6 acrylic stand",
+            "print_size": "10,5cm x 14,5cm",
+            "canvas_w": 1240,
+            "canvas_h": 1748,
+            "shape": {
+                "type": "rect",
+                "radius": 0
+            },
+            "logo": {
+                "w": 400,
+                "h": 200,
+                "offset_y": 0.1,
+                "offset_x": 0.5
+            },
+            "karmaLogo": {
+                "w": 400,
+                "h": 100,
+                "offset_y": 0.95,
+                "offset_x": 0.5
+            },
+            "title": {
+                "offset_x": 0.5,
+                "offset_y": 0.275,
+                "fontsize": 57
+            },
+            "subtitle": {
+                "offset_x": 0.5,
+                "offset_y": 0.84,
+                "fontsize": 14
+            },
+            "tableTitle": {
+                "offset_x": 0.76,
+                "offset_y": 0.58,
+                "fontsize": 16
+            },
+            "tableNumber": {
+                "offset_x": 0.76,
+                "offset_y": 0.68,
+                "fontsize": 49
+            },
+            "bottomTitle": {
+                "offset_x": 0.5,
+                "offset_y": 0.88,
+                "fontsize": 14
+            },
+            "QRCode": {
+                "size": 150,
+                "offset_x": 0.33,
+                "offset_y": 0.615
+            }
         }
-    },
-    "square1" : {
-        shape: {
-            type: "rect",
-            radius: 140
-        },
-        logo: {
-            w: 250,
-            h: 125,
-            offset_y: 30
-        },
-        title: {
-            offset_x: radius,
-            offset_y: radius-225,
-            fontsize: 40
-        },
-        subtitle : {
-            offset_x: radius,
-            offset_y: radius-10,
-            fontsize: 12
-        },
-        tableTitle : {
-            offset_x: radius + radius/centerRadiusOffset,
-            offset_y: radius+100,
-            fontsize: 16
-        },
-        tableNumber : {
-            offset_x: radius + radius/centerRadiusOffset,
-            offset_y: radius+250,
-            fontsize: 49
-        },
-        bottomTitle : {
-            offset_x: radius,
-            offset_y: radius*2-110,
-            fontsize: 12
-        },
-        QRCode : {
-            offset_x: radius,
-            offset_y: radius-10,
-        }
-    }
 }
 
 let tempImage = new Image;
@@ -100,8 +184,6 @@ tempImage.onload = function () {
     updateCode();
 }
 
-tempImage.src = "https://raw.githubusercontent.com/karmadev/website-additions/master/images/powered.png";
-
 WebFont.load({
     google: {
         families: ['Inter:500,700,800,900']
@@ -109,11 +191,28 @@ WebFont.load({
 });
 
 WebFontConfig = {
-    active: updateTexts()
+    active: init()
 };
 
-let imageLoader = document.getElementById('imageLoader');
-imageLoader.addEventListener('change', handleImage, false);
+function init() {
+    //Connect the file upload step
+    let imageLoader = document.getElementById('imageLoader');
+    imageLoader.addEventListener('change', handleImage, false);
+
+    //Prep the templates
+    let container = document.getElementById("designOptions");
+    container.innerHTML = "";
+    Object.keys(defaults).forEach(val => {
+        let design = defaults[val];
+        container.innerHTML += `<div><input type='radio' id='${val}' name='design' value='${val}' checked><label for='${val}'>${design.name}</label> <span style='font-size:12px;color:#aaa;'>${design.print_size}</span></div>`;
+    })
+
+    //Update the texts
+    updateTexts();
+
+    //Load the Karma logo - this will trigger the onloaded function that will render the code
+    tempImage.src = "https://raw.githubusercontent.com/karmadev/website-additions/master/images/powered.png";
+}
 
 const coverImg = (img, type, winW, winH, offsetX, offsetY, context) => {
     const imgRatio = img.height / img.width
@@ -137,18 +236,26 @@ function drawImage() {
     if(imageLogo != null) {
         let w = def.logo.w;
         let h = def.logo.h;
-        let offsetY = def.logo.offset_y;
-        ctx.fillRect(size/2-w/2, offsetY, w, h);
-        coverImg(imageLogo, 'contain', w, h, size/2-w/2, offsetY, ctx);
+
+        //Recalc the offset in %
+        let offsetY = def.logo.offset_y * canvas.height;
+        let offsetX = def.logo.offset_x * size;
+
+        ctx.fillRect(offsetX-w/2, offsetY-h/2, w, h);
+        coverImg(imageLogo, 'contain', w, h, offsetX-w/2, offsetY-h/2, ctx);
     }
 
     //Draw the Karma logotype
     if(karmaLogo != null) {
-        let w = 200;
-        let h = 70;
-        let offsetY = size - 100;
-        ctx.fillRect(size/2-w/2, offsetY, w, h);
-        coverImg(karmaLogo, 'contain', w, h, size/2-w/2, offsetY, ctx);
+        let w = def.karmaLogo.w;
+        let h = def.karmaLogo.h;
+
+        //Recalc the offset in %
+        let offsetY = def.karmaLogo.offset_y * canvas.height
+        let offsetX = def.karmaLogo.offset_x * size
+
+        ctx.fillRect(offsetX-w/2, offsetY-h/2, w, h);
+        coverImg(karmaLogo, 'contain', w, h, offsetX-w/2, offsetY-h/2, ctx);
     }
 }
 
@@ -273,16 +380,25 @@ function updateTexts() {
     updateCode();
 }
 
-function updateCode(updateCodeToo = true) {
+function updateCode() {
+
+    //Find out which style we're using
+    let selected = document.querySelector('input[name="design"]:checked').value;
+    let def = defaults[selected];
+
+    //Update the canvas size
+    canvas.width = def.canvas_w;
+    canvas.height = def.canvas_h;
+
+    size = canvas.width;
+    radius = size/2;
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     let bgColor = document.getElementById("backgroundColor").value;
     let textColor = document.getElementById("textColor").value;
 
-    //Find out which style we're using
-    let selected = document.querySelector('input[name="design"]:checked').value;
-    let def = defaults[selected];
+
 
     //Draw the circle
     ctx.fillStyle = bgColor;
@@ -293,7 +409,7 @@ function updateCode(updateCodeToo = true) {
         ctx.closePath();
         ctx.fill();
     } else {
-        roundRect(ctx,0,0,size, size, def.shape.radius, true, false);
+        roundRect(ctx,0,0,canvas.width, canvas.height, def.shape.radius, true, false);
         title = "textTitleSquare";
     }
 
@@ -323,31 +439,31 @@ function updateCode(updateCodeToo = true) {
     ctx.font = 'bold ' + def.title.fontsize * sizeMultiplier + 'px inter';
     ctx.textAlign = 'center'
     ctx.fillStyle = textColor;
-    writeText(text, def.title.offset_x, def.title.offset_y, def.title.fontsize);
+    writeText(text, def.title.offset_x * size, def.title.offset_y * canvas.height, def.title.fontsize);
 
     //Generate the subtitle
     text = getText("textSubtitle");
     ctx.font = '500 ' + def.subtitle.fontsize * sizeMultiplier + 'px inter';
     ctx.textAlign = 'center'
-    writeText(text, def.subtitle.offset_x, def.subtitle.offset_y, def.subtitle.fontsize);
+    writeText(text, def.subtitle.offset_x * size, def.subtitle.offset_y * canvas.height, def.subtitle.fontsize);
 
     //Generate the bottom text
     text = getText("textBottom");
     ctx.font = '500 ' + def.bottomTitle.fontsize * sizeMultiplier + 'px inter';
     ctx.textAlign = 'center'
-    writeText(text, def.bottomTitle.offset_x, def.bottomTitle.offset_y, def.bottomTitle.fontsize);
+    writeText(text, def.bottomTitle.offset_x * size, def.bottomTitle.offset_y * canvas.height, def.bottomTitle.fontsize);
 
     //Generate the table text
     text = getText("textTableTitle");
     ctx.font = '500 ' + def.tableTitle.fontsize * sizeMultiplier + 'px inter';
     ctx.textAlign = 'center'
-    writeText(text, def.tableTitle.offset_x, def.tableTitle.offset_y, def.tableTitle.fontsize);
+    writeText(text, def.tableTitle.offset_x * size, def.tableTitle.offset_y * canvas.height, def.tableTitle.fontsize);
 
     //Generate the table number text
     text = document.getElementById("textTableNumber").value;
     ctx.font = 'bold ' + def.tableNumber.fontsize * sizeMultiplier + 'px inter';
     ctx.textAlign = 'center'
-    writeText(text, def.tableNumber.offset_x, def.tableNumber.offset_y, def.tableNumber.fontsize);
+    writeText(text, def.tableNumber.offset_x * size, def.tableNumber.offset_y * canvas.height, def.tableNumber.fontsize);
 
     //Generate a base QR-code
     updateQR();
@@ -362,11 +478,17 @@ function writeText(txt, x, y, fontSize = 0) {
 }
 
 function updateQR() {
-    const radius = canvas.width/2;
+
+    //Find out which style we're using
+    let selected = document.querySelector('input[name="design"]:checked').value;
+    let def = defaults[selected];
+
     let text = document.getElementById("storefrontURL").value;
 
     let bgColor = document.getElementById("backgroundColor").value;
     let qrColor = document.getElementById("qrColor").value;
+
+    let qrSize = def.QRCode.size * sizeMultiplier;
 
     console.log(qrColor);
 
@@ -376,12 +498,10 @@ function updateQR() {
         level: 'H',
         padding: 0,
         value: text.toString(),
-        size: 76.235*sizeMultiplier
+        size: qrSize
     });
 
-    let offset = 160;
-
-    ctx.drawImage(qr.image,radius - radius/centerRadiusOffset - qr.size/2,radius - radius/2 + qr.size/2 + offset);
+    ctx.drawImage(qr.image,def.QRCode.offset_x*size - qrSize/2, def.QRCode.offset_y*canvas.height - qrSize/2);
     //document.getElementById("debug").innerText = text;
 }
 
